c82a058237b5e9195455f025c14c7eec45efa035
Carlos Alberto <carliinhos1994@gmail.com>
2025-11-08 15:58:22 -0300

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello wordl!");
+
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com Sucesso");
+    }
+    else
+        return Results.Unauthorized();
+
+});
+
+
+
+app.Run();
+
